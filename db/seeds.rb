581844e544ca865ab6f693428ea6177849d33c84
sargonas <sargonas@gmail.com>
2016-04-29 19:35:41 +0000

    created database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Filament.create(name: 'CF White 1', material: 'PLA', color: 'white', length: 90000, weight: 750, cost: 25.00)
+Filament.create(name: 'CF Black 1', material: 'PLA', color: 'black', length: 90000, weight: 750, cost: 25.00)
+Filament.create(name: 'CF nGen Black Sample 1', material: 'nGen', color: 'black', length: 900, weight: 150, cost: 5.00)
+Filament.create(name: 'CF XTC Clear 1', material: 'XTC', color: 'clear', length: 90000, weight: 750, cost: 40.00)
+Print.create(name: 'Marvin Test', length: 500, weight: 10, price: 5.00, filament_id: 3)
+Print.create(name: 'Nes Pi Case', length: 1500, weight: 80, price: 15.00, filament_id: 1)
