d99ad9e4dda7e9ae977dc3abb416e6511814266d
sargonas <sargonas@gmail.com>
2016-05-03 18:59:05 +0000

    added some basic testing for user and index pages... just in case.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "should not get index unless logged in" do
+    get :index
+    assert_response :redirect
+  end
+  
+  test "should get index when logged in" do
+    sign_in User.first
     get :index
     assert_response :success
   end
-
 end
