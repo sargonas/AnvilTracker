b9615d1b04e4654c63ca12f9176edf363445b873
sargonas <sargonas@gmail.com>
2016-05-31 22:49:18 +0000

    code documentation pass because I was lazy before :)

--- app/controllers/filaments_controller.rb
+++ app/controllers/filaments_controller.rb
@@ -1,5 +1,7 @@
 class FilamentsController < ApplicationController
     helper_method :sort_column, :sort_direction
+    
+    #CSV import logic
     def import
         Filament.import(params[:file])
         redirect_to root_url, notice: "Filaments imported."
@@ -57,6 +59,7 @@ class FilamentsController < ApplicationController
             params.require(:filament).permit(:name, :material, :color, :length, :weight, :cost, :diameter, :archived)
         end
         
+        #extras to support the sorting of the index table
         def sort_column
             Filament.column_names.include?(params[:sort]) ? params[:sort] : "id"
         end
--- app/controllers/prints_controller.rb
+++ app/controllers/prints_controller.rb
@@ -1,5 +1,6 @@
 class PrintsController < ApplicationController
     helper_method :sort_column, :sort_direction
+    #CSV import support
     def import
         Print.import(params[:file])
         redirect_to root_url, notice: "Prints imported."
@@ -20,6 +21,7 @@ class PrintsController < ApplicationController
     
     def edit
         @print = Print.find(params[:id])
+        #does archived false belong here? let's investigate some time
         @filament_options = Filament.where(:archived => false).map{ |f| [ f.name, f.id ] }
     end
     
@@ -54,6 +56,7 @@ class PrintsController < ApplicationController
         def print_params
             params.require(:print).permit(:name, :length, :weight, :price, :filament_id, :printed_date, :volume, :extruder_id, :print_time)
         end
+        #controls to help with sorting the indexes
         def sort_column
             Print.column_names.include?(params[:sort]) ? params[:sort] : "id"
         end
