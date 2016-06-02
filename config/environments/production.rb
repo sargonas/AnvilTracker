5e5927ce6df7127b4a94212b6347b8bfe5cac9ef
sargonas <sargonas@gmail.com>
2016-06-02 04:22:27 +0000

    fix for broken Production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   #mailer info
+  config.action_mailer.default_url_options = { :host => 'anvil.thevoxelforge.com' }
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
